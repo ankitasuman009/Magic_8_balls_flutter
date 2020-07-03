ed33a62f0b880a1c486a8cd88c4da2df57ea066a
ankitasuman009 <ankitasuman8667@gmail.com>
2020-07-02 19:18:40 -0800

    inital commit

---
+++ README.md
@@ -0,0 +1,21 @@
+![App Brewery Banner](https://github.com/ankitasuman009/images/blob/master/ask_me_ath_image.png)
+
+
+# Magic 8 Ball 🎱
+Boss level challenge 2
+
+## Our Goal
+
+A magical 8 ball app, it will give you the answers to all the tricky questions in life. You can make the ball change at the press of a button. 
+
+
+
+## Concept cover
+Use of those concept learned so far...
+
+
+![Finished App](https://github.com/londonappbrewery/Images/blob/master/8-ball-flutter-gif.gif)
+
+
+
+
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+        home: BallPage(),
+      ),
+    );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int n = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: FlatButton(
+          onPressed: () {
+            setState(() {
+              n = Random().nextInt(4) + 1;
+            });
+          },
+          child: Image.asset("images/ball$n.png"),
+        ),
+      ),
+    );
+  }
+}
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Center(
+          child: Text("ASK ME ANYTHING"),
+        ),
+      ),
+      backgroundColor: Colors.blue,
+      body: Ball(),
+    );
+  }
+}
